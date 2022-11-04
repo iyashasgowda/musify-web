7988655e472250ce011def8d318f7e1a13c242c8
Yashas Gowda <yashas348@gmail.com>
2022-11-04 17:34:12 +0530

    #revoking gh-pages for later deployment
    Signed-off-by: Yashas Gowda <yashas348@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import Menu from './components/Menu';
+import Body from './components/Body';
+import Strip from './components/MusicStrip';
 import './styles/app.css';
 
-function App() {
-  return (
-    <div className="main-container"></div>
-  );
+const App = () => {
+    return (
+        <div className="main-container">
+            <Menu/>
+            <Body/>
+            <Strip/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/MusicStrip.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/music-strip.css'
+
+const MusicStrip = () => {
+    return (
+        <div className="music-strip-container">
+            <div className="music-strip">
+
+            </div>
+        </div>
+    );
+};
+
+export default MusicStrip;
